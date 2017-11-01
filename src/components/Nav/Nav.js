903eb8e1a177d9a87b6a8dddad40b9732a830563
Aidan Bane <aidanfbane@gmail.com>
2017-10-31 23:00:18 -0700

    (feat) Title component added

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Title from './Title';
 import './Nav.css';
 
 const Nav = () => (
   <nav className="Nav">
+    <Title />
     <Link className="Nav__link" to="/">
-      FreeReader
+      Reader
     </Link>
     <Link className="Nav__link" to="/profile">
       Profile
---
+++ src/components/Nav/Title.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Title.css';
+
+const Title = () => {
+  return <h1 className="Title">FreeReader</h1>;
+};
+
+export default Title;
